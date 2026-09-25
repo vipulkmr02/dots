f3b6e1dcb1ba976e6e24966d22e9dd9870ddf2c0
Vipul Kumar Gupta <57555987+vipulkmr02@users.noreply.github.com>
2026-09-26 04:54:52 +0530

    add sane hardtime.nvim config

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,14 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    max_count = 5,
+    disable_mouse = false,
+    hint = true,
+    notification = true,
+    restriction_mode = "hint",
+  },
+}
